43d2720a98449d8effdc8890b8e99590941397aa
Edelgard <jtboyman@gmail.com>
2022-05-19 19:05:35 -0400

    test data successfully retrieved from db, connection working

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {initializeApp} from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -23,7 +24,27 @@ const firebaseConfig = {
   appId: "1:864174371592:web:aed9687e4872e139d3b2bd"
 };
 
+//initialize firebase app
 initializeApp(firebaseConfig);
+
+//initialize firestore service
+const db = getFirestore();
+
+//reference the collection from the db
+const colRef = collection(db, "users");
+
+//get data from collection
+getDocs(colRef).then((snapshot) => {
+  let users: {}[] = [];
+  snapshot.docs.forEach((doc) => {
+    users.push({ ...doc.data(), id: doc.id})
+  })
+  console.log(users)
+})
+.catch(err => {
+  console.log(err.message)
+});
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
